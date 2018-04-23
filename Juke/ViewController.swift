9080d9f4061fb3a2f32b0a24d007e2219d5b3e8f
Marcus Rogers <Marcus@marcuss-macbook-pro.local>
2018-04-23 14:01:54 -0700

    Test 2

--- Juke/ViewController.swift
+++ Juke/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     // HEY EVERYONE
     
+    // MARCUS WAS HERE
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
